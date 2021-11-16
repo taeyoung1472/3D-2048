47e9a22184d2ef59f480dfeebf70a7fe984be21e
박상빈 <jixg20@gmail.com>
2021-11-17 02:06:23 +0900

    다트윈 추가, 시작화면에 닉네임 넣는곳 추가

---
+++ Assets/1.Scripts/Manager/NameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class NameManager : MonoBehaviour
+{
+    [Header("Name")]
+    [SerializeField]
+    private InputField nameInput;
+
+    [SerializeField]
+    private int nameLimit;
+
+    private IEnumerator changeCoroutine;
+    private Text namePlaceholder;
+
+    private string nickName;
+    private bool isShaking;
+    private float dotweenDuration = 0.5f;
+
+    #region 이벤트
+    private void Start()
+    {
+        namePlaceholder = nameInput.placeholder.GetComponent<Text>();
+        nameInput.characterLimit = nameLimit;
+        changeCoroutine = ChangePlaceholder();
+        StartCoroutine(changeCoroutine);
+    }
+    #endregion
+
+    #region OnClick
+    public void OnClickStart()
+    {
+        nickName = nameInput.text;
+        Debug.Log("함수 밖");
+        if (isShaking) return;
+        nickName = nickName.Trim();
+        nickName = nickName.Replace("\n", "").Replace("\r", "").Replace("ㅤ", "");
+        if (nickName.Length <= 0)
+        {
+            if (isShaking) return;
+            isShaking = true;
+            StartCoroutine(ShakeText());
+        }
+    }
+    #endregion
+
+    private IEnumerator ShakeText()
+    {
+        if (changeCoroutine != null)
+        {
+            Debug.Log(changeCoroutine);
+
+            StopCoroutine(changeCoroutine);
+        }
+        Debug.Log("함수 안");
+
+        nameInput.text = "";
+        namePlaceholder.color = Color.red;
+        namePlaceholder.text = "닉네임을 잘못입력하셨습니다";
+
+        namePlaceholder.transform.DOShakePosition(dotweenDuration, 15f, 20, 45f);
+
+        yield return new WaitForSeconds(dotweenDuration);
+
+        namePlaceholder.color = Color.gray;
+        namePlaceholder.text = "닉네임을 입력해주세요";
+
+        StartCoroutine(changeCoroutine);
+        isShaking = false;
+    }
+    private IEnumerator ChangePlaceholder()
+    {
+        char comma = '.';
+        string commas = "";
+        while (true)
+        {
+            commas = ".";
+            for (int i = 1; i <= 3; i++)
+            {
+                yield return new WaitWhile(() => nameInput.isFocused);
+                namePlaceholder.text = "닉네임을 입력해주세요" + commas;
+                commas += comma;
+                yield return new WaitForSeconds(0.6f);
+            }
+        }
+    }
+}
