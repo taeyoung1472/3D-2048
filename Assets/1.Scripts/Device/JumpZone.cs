15a575ab53dbee64c1532de075af12db007b3216
박상빈 <jixg20@gmail.com>
2022-02-11 11:08:03 +0900

    소리추가

--- Assets/1.Scripts/Device/JumpZone.cs
+++ Assets/1.Scripts/Device/JumpZone.cs
@@ -6,12 +6,16 @@ public class JumpZone : MonoBehaviour
 {
     [SerializeField]
     private float force = 5f;
-
+    [SerializeField]
+    private AudioClip audioClip;
+    [SerializeField]
+    private AudioSource audioSource;
 
     private void OnCollisionEnter(Collision other) {
         IJumpable jumpable = other.gameObject.GetComponent<IJumpable>();
 
         if(jumpable == null)return;
+        audioSource.PlayOneShot(audioClip);
         jumpable.Jump(this,force, transform.up);
     }
 }
